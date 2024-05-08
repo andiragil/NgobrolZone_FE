c28b42be1eb99ae990522ea68463e110299fef33
stefhan235 <stefhansalim12@gmail.com>
2024-05-08 21:53:14 +0700

    home feature

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,5 @@
-import React from 'react'
+import React from "react";
 
 export const HomePage = () => {
-    return (
-        <div>HomePage</div>
-    )
-}
+    return <div>HomePage Chat App</div>;
+};
