645054af12145523863fd08cc1f6b0658bce5ec1
andragil <andiragilw09@gmail.com>
2024-05-08 21:54:47 +0700

    feature regis

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import './App.css'
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { HomePage } from "./pages/HomePage";
+import { RegisPage } from "./pages/RegisPage"
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <HomePage />,
+  },
+  {
+    path: "/",
+    element: <RegisPage />,
   },
 ])
 
