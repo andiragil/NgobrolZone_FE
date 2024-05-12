e7c43a41ef03ac61dc48bf4d4b2096d4c7aa0b38
PrakashFilemon <137614814+PrakashFilemon@users.noreply.github.com>
2024-05-12 14:18:55 +0700

    Feature login (#6)
    * featured : login
    
    * Add Login Page
    
    * Change Vercel.json

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,28 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import RegisPage from "./pages/RegisPage";
 import ProfilePage from "./pages/ProfilePage";
+import LoginPage from "./pages/LoginPage";
 
 const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <HomePage />,
-    },
-    {
-        path: "/register",
-        element: <RegisPage />,
-    },
-    {
-        path: "/profile",
-        element: <ProfilePage />,
-    },
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+  {
+    path: "/register",
+    element: <RegisPage />,
+  },
+  {
+    path: "/profile",
+    element: <ProfilePage />,
+  },
+  {
+    path: "/login",
+    element: <LoginPage />,
+  },
 ]);
 
 function App() {
-    return <RouterProvider router={router} />;
+  return <RouterProvider router={router} />;
 }
 export default App;
