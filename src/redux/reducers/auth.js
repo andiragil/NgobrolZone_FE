cdc65b34dd3e411697995616594141deb47e5413
andragil <andiragilw09@gmail.com>
2024-05-15 20:36:53 +0700

    add actions and reducers

---
+++ src/redux/reducers/auth.js
@@ -0,0 +1,31 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+// for state
+const initialState = {
+    token: localStorage.getItem("token") || null,
+    user: null,
+};
+
+// slice
+const authSlice = createSlice({
+    name: "auth",
+    initialState,
+    reducers: {
+        setToken: (state, action) => {
+            if (action.payload) {
+                localStorage.setItem("token", action.payload);
+            } else {
+                localStorage.removeItem("token");
+            }
+
+            state.token = action.payload;
+        },
+        setUser: (state, action) => {
+            state.user = action.payload;
+        },
+    },
+});
+
+
+export const { setToken, setUser } = authSlice.actions;
+export default authSlice.reducer;
